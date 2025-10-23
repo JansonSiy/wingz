bb8bc12c27f59054fb69c92cdaa4e34457b5fe97
jansonsiy-yeoki <131631080+jansonsiy-yeoki@users.noreply.github.com>
2025-10-23 17:36:04 +0800

    Created admin interface and updated models

--- rider/admin.py
+++ rider/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import User, Ride, RideEvent
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'role')
+    list_filter = ('role',)
+    search_fields = ('first_name', 'last_name', 'email')
+
+
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin):
+    list_display = ('get_rider_first_name', 'get_driver_first_name', 'status')
+    list_filter = ('status',)
+    search_fields = ('id_rider__first_name', 'id_rider__last_name', 'id_driver__first_name', 'id_driver__last_name', 'status')
+
+    def get_rider_first_name(self, obj):
+        return f'{obj.id_rider.first_name} {obj.id_rider.last_name}'
+    get_rider_first_name.short_description = 'Rider'
+
+    def get_driver_first_name(self, obj):
+        return f'{obj.id_driver.first_name} {obj.id_driver.last_name}'
+    get_driver_first_name.short_description = 'Driver'
+
+
+@admin.register(RideEvent)
+class RideEventAdmin(admin.ModelAdmin):
+    list_display = ('ride', 'description', 'created_at')
+    search_fields = ('ride__id_rider__first_name', 'ride__id_rider__last_name', 'ride__id_driver__first_name', 'ride__id_driver__last_name')
--- rider/models.py
+++ rider/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class User(models.Model):
     ROLE_CHOICES = [
+        ('admin', 'Admin'),
         ('rider', 'Rider'),
         ('driver', 'Driver'),
     ]
@@ -9,8 +10,12 @@ class User(models.Model):
     role = models.CharField(max_length=255, choices=ROLE_CHOICES)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    phone_number = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.role})'
+
 
 class Ride(models.Model):
     STATUS_CHOICES = [
@@ -28,7 +33,13 @@ class Ride(models.Model):
     dropoff_longitude = models.FloatField()
     pickup_time = models.DateTimeField()
 
+    def __str__(self):
+        return f'Driver: {self.id_driver.first_name} {self.id_driver.last_name} | Rider: {self.id_rider.first_name} {self.id_rider.last_name} | Status: {self.status}'
+
 class RideEvent(models.Model):
     ride = models.ForeignKey('Ride', on_delete=models.CASCADE)
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.ride} | {self.description[:20]}...'
