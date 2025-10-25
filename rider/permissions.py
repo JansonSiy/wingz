0a6280b4dacd8009d7563613008686d7ce1420d0
jansonsiy-yeoki <131631080+jansonsiy-yeoki@users.noreply.github.com>
2025-10-25 15:19:28 +0800

    Added loggers in permission check

--- rider/permissions.py
+++ rider/permissions.py
@@ -1,14 +1,24 @@
+import logging
 from rest_framework import permissions
 from .models import User
+
+logger = logging.getLogger(__name__)
+
 
 class IsAdminUserRole(permissions.BasePermission):
     def has_permission(self, request, view): # added user authentication
         if not request.user.is_authenticated:
+            logger.warning("Error: User not authenticated.")
             return False
         
         try:
             user = User.objects.get(email=request.user.email)
         except User.DoesNotExist:
+            logger.error(f"Error: User not found ({request.user.email})")
             return False
 
-        return user.role == "admin"
+        if user.role != "admin":
+            logger.error(f"Error: User is not an admin ({request.user.email})")
+            return False
+
+        return True
