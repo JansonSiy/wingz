3333468451df14ad06e5c536e5c1f8eba6e74543
jansonsiy-yeoki <131631080+jansonsiy-yeoki@users.noreply.github.com>
2025-10-25 14:40:01 +0800

    Add filter by pickup_time

--- rider/views.py
+++ rider/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 from .models import User, Ride, RideEvent
 from .serializers import UserSerializer, RideSerializer, RideEventSerializer
 from .permissions import IsAdminUserRole
@@ -8,11 +9,17 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAdminUserRole]
 
+
 class RideViewSet(viewsets.ModelViewSet):
     queryset = Ride.objects.all()
     serializer_class = RideSerializer
     permission_classes = [IsAdminUserRole]
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = ['status', 'id_rider__email']
+    ordering_fields = ['pickup_time']
+    ordering = ['pickup_time']
+    
 
 class RideEventViewSet(viewsets.ModelViewSet):
     queryset = RideEvent.objects.all()
